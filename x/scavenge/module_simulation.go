e995386955aa5db0fc7e6752633182683f052095
Hechen93 <48193329+Hechen93@users.noreply.github.com>
2022-01-03 13:05:50 -0700

    starport tutorial3

---
+++ x/scavenge/module_simulation.go
@@ -0,0 +1,78 @@
+package scavenge
+
+import (
+	"math/rand"
+
+	"github.com/cosmonaut/scavenge/testutil/sample"
+	scavengesimulation "github.com/cosmonaut/scavenge/x/scavenge/simulation"
+	"github.com/cosmonaut/scavenge/x/scavenge/types"
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
+)
+
+// avoid unused import issue
+var (
+	_ = sample.AccAddress
+	_ = scavengesimulation.FindAccount
+	_ = simappparams.StakePerAccount
+	_ = simulation.MsgEntryKind
+	_ = baseapp.Paramspace
+)
+
+const (
+	opWeightMsgSubmitScavenge = "op_weight_msg_create_chain"
+	// TODO: Determine the simulation weight value
+	defaultWeightMsgSubmitScavenge int = 100
+
+	// this line is used by starport scaffolding # simapp/module/const
+)
+
+// GenerateGenesisState creates a randomized GenState of the module
+func (AppModule) GenerateGenesisState(simState *module.SimulationState) {
+	accs := make([]string, len(simState.Accounts))
+	for i, acc := range simState.Accounts {
+		accs[i] = acc.Address.String()
+	}
+	scavengeGenesis := types.GenesisState{
+		// this line is used by starport scaffolding # simapp/module/genesisState
+	}
+	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(&scavengeGenesis)
+}
+
+// ProposalContents doesn't return any content functions for governance proposals
+func (AppModule) ProposalContents(_ module.SimulationState) []simtypes.WeightedProposalContent {
+	return nil
+}
+
+// RandomizedParams creates randomized  param changes for the simulator
+func (am AppModule) RandomizedParams(_ *rand.Rand) []simtypes.ParamChange {
+
+	return []simtypes.ParamChange{}
+}
+
+// RegisterStoreDecoder registers a decoder
+func (am AppModule) RegisterStoreDecoder(_ sdk.StoreDecoderRegistry) {}
+
+// WeightedOperations returns the all the gov module operations with their respective weights.
+func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
+	operations := make([]simtypes.WeightedOperation, 0)
+
+	var weightMsgSubmitScavenge int
+	simState.AppParams.GetOrGenerate(simState.Cdc, opWeightMsgSubmitScavenge, &weightMsgSubmitScavenge, nil,
+		func(_ *rand.Rand) {
+			weightMsgSubmitScavenge = defaultWeightMsgSubmitScavenge
+		},
+	)
+	operations = append(operations, simulation.NewWeightedOperation(
+		weightMsgSubmitScavenge,
+		scavengesimulation.SimulateMsgSubmitScavenge(am.accountKeeper, am.bankKeeper, am.keeper),
+	))
+
+	// this line is used by starport scaffolding # simapp/module/operation
+
+	return operations
+}
